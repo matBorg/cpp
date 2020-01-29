f9b1ac4d71b3537c048f3c82066d75a3b1300a27
Matteo Borgesano <matteoborgesano@MBPdiTelomerase.station>
2020-01-29 18:58:21 +0100

    Pronto per verifica

---
+++ verifica29_12_20/es.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <assert.h>
+
+// input data
+int N, M, s;
+
+int main() {
+  //Apertura dei file necessari impostandoli come standard input e standard output
+  freopen("./prova/esempio2.txt", "r", stdin);
+
+  //input da file
+    assert(1 == scanf("%d", &N));
+    int R;
+    for(int i=0; i<N; i++)
+  {
+      assert(1 == scanf("%d", &M));
+        for(int j=0; j<M; j++)
+      {
+        assert(1 == scanf("%d", &s));
+      R += s;
+    }
+  }
+  
+    //stampa del risultato
+  printf("%d ", R);
+}
---
+++ verifica29_12_20/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "libArray.h"
+using namespace std;
+// __________________________________________________________  
+//|                                                          |
+//|RICORDA DI CAMBIARE IL NUMERO DEL FILE AD ESERCIZIO FINITO|
+//|__________________________________________________________|
+
+int esercizio()
+int main(){
+  int N; 
+  int i;
+  
+  //esempi
+  //##########################################
+  //freopen("./prova/esempio1.txt","r",stdin);
+  //freopen("./prova/esempio2.txt","r",stdin);
+  //##########################################
+  
+
+  //dai file del prof
+  //##########################################
+  //freopen("./input/nesercizio","r",stdin); 
+  //freopen("./output/nesercizio","w",stdout);
+  //##########################################
+
+  assert(numerodivalori==scanf("%d",&N)); 
+  int L[N]; 
+  for(int i=0; i<N;i++){
+    assert(1==scanf("%d",&L[i]));
+    }
+  
+}
+
+
+
+/*
+  per scanf()
+  %d interi,
+  f float,
+  c char
+  s stringhe,
+  dopo la virgola metti tante variabili con prima la & quanti i valori inseriti prima (stessa cosa per la d)
+  */
+
